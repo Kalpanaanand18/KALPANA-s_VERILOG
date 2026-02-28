449260334b1058f17711a405beae28484bd7c4ba
Kalpanaanand18 <kalpana.amudha181105@gmail.com>
2026-02-28 15:45:36 +0530

    Create 07.XNOR_GATE.v

---
+++ GATES/07.XNOR_GATE.v
@@ -0,0 +1,24 @@
+module xnorg(A,B,Y);
+input A,B;
+output Y;
+  assign Y =~(A^B);
+endmodule
+module xnorg_tb();
+reg A,B;
+wire Y;
+  xnorg xnorgate(.A(A),.B(B),.Y(Y));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,xnorg_tb);
+  end
+initial begin
+    A=1'b0;B=1'b0;
+    #10 A=1'b0;B=1'b1;
+    #10 A=1'b1;B=1'b0;
+    #10 A=1'b1;B=1'b1;
+    #10 $finish;
+end
+  always@(*) begin
+    $display("time=%0t  A=%b B=%b Y=%b",$time,A,B,Y);
+  end
+

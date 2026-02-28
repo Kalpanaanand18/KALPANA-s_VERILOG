fe02e3c80727183012ee8dacd9175e94eeeb7e14
Kalpanaanand18 <kalpana.amudha181105@gmail.com>
2026-02-28 15:42:05 +0530

    Update 02.AND_GATE.v

--- GATES/02.AND_GATE.v
+++ GATES/02.AND_GATE.v
@@ -1,1 +1,25 @@
+module andg(A,B,Y);
+input A,B;
+output Y;
+assign Y = A & B;
+endmodule
+module andg_tb();
+reg A,B;
+wire Y;
+  andg andgate(.A(A),.B(B),.Y(Y));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,andg_tb);
+  end
+initial begin
+    A=1'b0;B=1'b0;
+    #10 A=1'b0;B=1'b1;
+    #10 A=1'b1;B=1'b0;
+    #10 A=1'b1;B=1'b1;
+    #10 $finish;
+end
+  always@(*) begin
+    $display("time=%0t  A=%b B=%b Y=%b",$time,A,B,Y);
+  end
+endmodule
 

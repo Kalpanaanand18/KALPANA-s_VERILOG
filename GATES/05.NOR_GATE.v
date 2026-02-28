fa8f72e1ea577deba69c262faa69bfd04ebab804
Kalpanaanand18 <kalpana.amudha181105@gmail.com>
2026-02-28 15:43:25 +0530

    Create 05.NOR_GATE.v

---
+++ GATES/05.NOR_GATE.v
@@ -0,0 +1,24 @@
+module norg(A,B,Y);
+input A,B;
+output Y;
+assign Y = ~(A | B);
+endmodule
+module norg_tb();
+reg A,B;
+wire Y;
+  norg norgate(.A(A),.B(B),.Y(Y));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,norg_tb);
+  end
+initial begin
+    A=1'b0;B=1'b0;
+    #10 A=1'b0;B=1'b1;
+    #10 A=1'b1;B=1'b0;
+    #10 A=1'b1;B=1'b1;
+    #10 $finish;
+end
+  always@(*) begin
+    $display("time=%0t  A=%b B=%b Y=%b",$time,A,B,Y);
+  end
+endmodule
